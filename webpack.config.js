925f8504cf4b79b4ccb32a234ab82ca64b21ff21
Les Chinyanga <les.chinyanga@gmail.com>
2024-10-20 23:21:26 +0200

    feat: initial webpack config

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  mode: "development",
+  devServer: {
+    static: "./dist",
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+};
