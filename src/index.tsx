31200e2af1e379840b0cb218a1c7ec7407af6d48
Les Chinyanga <les.chinyanga@gmail.com>
2024-10-21 06:57:12 +0200

    fix: update index tsx to resolve react render problem

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store/store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createRoot } from "react-dom/client";
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement!);
 
-ReactDOM.render(
+const queryClient = new QueryClient();
+
+root.render(
   <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </Provider>
 );
